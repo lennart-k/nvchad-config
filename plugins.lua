23b372e084a8f3e4b3e70408cca13cb3f9ff9661
Lennart <18233294+lennart-k@users.noreply.github.com>
2023-06-17 12:18:48 +0200

    add markdown-preview.nvim

--- plugins.lua
+++ plugins.lua
@@ -125,5 +125,10 @@ local plugins = {
       }
     }
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    ft = { "markdown" },
+  }
 }
 return plugins
